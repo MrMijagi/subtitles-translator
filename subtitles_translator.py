87f2640221949944214a623652615733b72f8855
MrMijagi <dawid.bilinski.99@gmail.com>
2020-12-01 16:19:25 +0100

    updated translator API + added better language selection

--- subtitles_translator.py
+++ subtitles_translator.py
@@ -1,14 +1,23 @@
 import sys
-from googletrans import Translator
+import os.path
+from google_trans_new import google_translator
 
-r = open(sys.argv[1], 'r', encoding='utf-8')
-w = open(sys.argv[1].replace('.sbv', '_en.sbv'), 'w', encoding='utf-8')
-translator = Translator(service_urls=['translate.google.com'])
+if len(sys.argv) != 3:
+    sys.exit("Wrong number of arguments! Check out readme.md for proper usage.")
+
+if not os.path.isfile(sys.argv[2]):
+    sys.exit("Provided file does not exist!")
+
+index_of_dot = sys.argv[2].rfind('.')
+file_extension = sys.argv[2][index_of_dot:]
+
+r = open(sys.argv[2], 'r', encoding='utf-8')
+w = open(sys.argv[2].replace(file_extension, '_' + sys.argv[1] + file_extension), 'w', encoding='utf-8')
+translator = google_translator()
 
 for line in r:
-    if line == "\n":
-        w.write(line)
-    else:
-        line += translator.translate(line, src='pl', dest='en').text + '\n'
-        w.write(line)
-    #print(line)
+    print(repr(line))
+    if line not in ["\n", ""]:
+        line += translator.translate(line, lang_tgt=sys.argv[1]) + '\n'
+
+    w.write(line)
